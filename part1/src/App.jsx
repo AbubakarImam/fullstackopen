84426af4c9dc17ebcf2132f1374ca70ff439dfd9
AbubakarImam <salamabubakarimam@gmail.com>
2023-10-18 08:32:12 +0100

    modify so as not to use map method

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,49 +1,72 @@
-function Part(props) {
-  return (
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
-}
-function Content(props) {
-  return (
-    <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
-    </div>
-  )
-}
 
-function Header(props) {
-  return (
-    <div>{props.course}</div>
-  )
-}
-
-function Total(props) {
-  return (
-    <div>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</div>
-  )
-}
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course.name} />
+      <Content part={course.parts} />
     </div>
   )
 }
 
 export default App
+
+function Header(props) {
+  console.log("header ", props)
+  return (
+    <div>{props.course}</div>
+  )
+}
+
+function Content(props) {
+  console.log("content ", props.part)
+  console.log(typeof (props.part))
+  const [a, b, c] = props.part
+  console.log(a);
+  return (
+    <div>
+      <Part part={a} />
+      <Part part={b} />
+      <Part part={c} />
+    </div>
+  )
+}
+
+function Part(props) {
+  console.log("part ", props)
+  const { name, exercises } = props.part;
+  return (
+    <div>
+      <div>
+        <p>{name}</p>
+        <Total exercises={exercises} />
+      </div>
+
+
+    </div>
+  )
+}
+
+
+function Total(props) {
+  console.log("total ", props)
+  return (
+    <div>Number of exercises:{props.exercises}</div>
+  )
+}
