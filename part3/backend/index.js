b9aa8b6fb4c0f589385a21eb007b84d0652a07f1
AbubakarImam <salamabubakarimam@gmail.com>
2023-10-25 15:17:48 +0100

    phonebooke backend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -42,8 +44,33 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
+const generatedId = () => {
+    const maxId = notes.length > 0 ?
+        Math.max(...notes.map(n => n.id))
+        : 0
+
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generatedId()
+    }
+
+    notes = notes.concat(note)
+    console.log(note)
+    response.json(note)
+})
 
 const PORT = 3001
 app.listen(PORT)
-console.log(`Server running on port ${PORT
-    }`)
+console.log(`Server running on port ${PORT}`)
