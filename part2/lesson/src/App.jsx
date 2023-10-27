a8bdf5bcea509374a2f611303652b71245007d2c
AbubakarImam <salamabubakarimam@gmail.com>
2023-10-27 20:47:49 +0100

    production version works locally

--- part2/lesson/src/App.jsx
+++ part2/lesson/src/App.jsx
@@ -1,28 +1,30 @@
-import { useState, useEffect } from "react"
-import Note from "./component/Note"
+import { useState, useEffect } from 'react'
+import Note from './component/Note'
+import Notification from './component/Notification'
+import Footer from './component/Footer'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    console.log('effect')
     noteService
       .getAll()
       .then(initialNotes => {
         setNotes(initialNotes)
       })
   }, [])
-  console.log('render', notes.length, 'notes');
 
-  const addNote = (e) => {
-    e.preventDefault()
+  const addNote = (event) => {
+    event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5
+      important: Math.random() > 0.5,
     }
+
     noteService
       .create(noteObject)
       .then(returnedNote => {
@@ -31,48 +33,59 @@ const App = () => {
       })
   }
 
-  const handleNoteChange = (e) => {
-    setNewNote(e.target.value)
-    console.log(e.target.value);
-  }
-
-  const toggleImportanceOf = (id) => {
+  const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
+
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
-      .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+      .catch(() => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
-        setNotes(notes.filter(n => n.id !== id))
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       })
-    console.log(`importance of ${id} needs to be toggled`)
   }
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
-
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 
 let notes = [
@@ -74,7 +75,7 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
